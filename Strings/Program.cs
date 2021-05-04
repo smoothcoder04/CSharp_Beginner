704adbef5b1fa40a9ff334828415ba217fd4689e
smoothcoder04 <70938130+smoothcoder04@users.noreply.github.com>
2021-05-04 11:56:35 +0100

    Uploaded local strings code

---
+++ Strings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Strings
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
