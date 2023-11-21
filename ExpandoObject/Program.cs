fa36e3fe85b32da3a4427dabaa4d98bc65a94263
mithu-mawhinney <mithumk4@gmail.com>
2023-11-21 13:49:04 +0000

    Garbage Collection

---
+++ ExpandoObject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Dynamic;
+
+namespace DynamicExpandoObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic car = new ExpandoObject();
+
+            car.Model = "Vauxahll";
+            car.Colour = "Topaz";
+
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Colour: {car.Colour}");
+
+            car.Accelerate = new Action(() => { Console.WriteLine("Accelerating"); });
+
+            car.Accelerate();
+
+            //delegate that returns a value
+            car.GetFuelEconomy = new Func<int>(() => { return 30; });
+            int mycarfueleconomy = car.GetFuelEconomy();
+
+            Console.WriteLine($"My car's range is {mycarfueleconomy} miles per gallon");
+
+        }
+    }
+}
---
+++ GarbageCollector/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GarbageCollector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first = "First Object";
+            string second = "Second Object";
+            string third = "Third Object";
+            string fourth = "Fourth Object";
+            string fifth = "Fifth Object";
+
+            //check the generation of the objects
+            Console.WriteLine(GC.GetGeneration(first));
+            Console.WriteLine(GC.GetGeneration(second));
+            Console.WriteLine(GC.GetGeneration(third));
+            Console.WriteLine(GC.GetGeneration(fourth));
+            Console.WriteLine(GC.GetGeneration(fifth));
+
+            //delete the reference to some of the objects
+            second = null;
+            fourth = null;
+
+            //start garbage collection and wait till it is done
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.WaitForPendingFinalizers();
+
+            //check the generation that survived
+            Console.WriteLine(GC.GetGeneration(first));
+            Console.WriteLine(GC.GetGeneration(third));
+            Console.WriteLine(GC.GetGeneration(fifth));
+        }
+    }
+}
