e5e2dac6020651ae7e1ed6cf0189cb0987580fa7
mithu-mawhinney <mithumk4@gmail.com>
2023-11-22 17:22:41 +0000

    Regular Expression

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Heelllooo World!! I am using this string to store some valuable information.";
+            var regexRule = new Regex((@"(\w*)or(\w*)"));
+            var matches = regexRule.Matches(text);
+
+            if (matches.Count > 0)
+            {
+                foreach (var match in matches)
+                {
+                    Console.WriteLine(match);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No matches found!!");
+            }
+        }
+    }
+}
